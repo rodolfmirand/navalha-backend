8e84fde6d1b2c0c8b9640bc32386d8b11d9dc700
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-06-12 19:47:02 -0300

    feat: add barbershop orm model class

---
+++ src/infrastructure/persistence/typeorm/models/barbershop.model.ts
@@ -0,0 +1,53 @@
+import { Column, Entity, OneToMany, OneToOne, PrimaryColumn } from "typeorm";
+import { UserModel } from "./user.model";
+import { BarberModel } from "./barber.model";
+import { ServiceModel } from "./service.model";
+
+@Entity({ name: 'barbershops' })
+export class BarbershopModel {
+    @PrimaryColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column({type: 'text', nullable: true})
+    description?: string;
+
+    @Column({nullable: true})
+    logoUrl?: string;
+
+    @Column({type: 'jsonb'})
+    address: {
+        street: string;
+        city: string;
+        disctric: string;
+        number: string;
+        state: string;
+    };
+
+    @Column()
+    contactPhone: string;
+
+    @Column({unique: true})
+    contactEmail: string;
+
+    @Column('uuid')
+    ownerId: string;
+
+
+    @OneToOne(() => UserModel)
+    owner: UserModel;
+
+    @OneToMany(()=> BarberModel, (barber) => barber.barbershop)
+    barbers: BarberModel[];
+
+    @OneToMany(() => ServiceModel, (service) => service.barbershop)
+    services: ServiceModel[];
+
+    @OneToMany(() => OperatingHoursModel, (hours) => hours.barbershop)
+    operatingHours: OperatingHoursModel[];
+
+    @OneToMany(() => AppointmentModel, (appointment) => appointment.barbershop)
+    appointments: AppointmentModel[];
+}
