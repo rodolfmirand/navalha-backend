0242476595b91390decd52ebb7fd2728cb5aa21e
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-06-12 12:08:25 -0300

    feat(model): add client orm class

---
+++ src/infrastructure/persistence/typeorm/models/client.model.ts
@@ -0,0 +1,33 @@
+import { Column, Entity, JoinColumn, OneToMany, OneToOne, PrimaryColumn } from "typeorm";
+import { UserModel } from "./user.model";
+import { Appointment } from "src/domain/entities/appointment.entity";
+
+@Entity({ name: 'clients' })
+export class ClientModel {
+    @PrimaryColumn('uuid')
+    id: string;
+
+    @Column('uuid')
+    userId: string;
+
+    @OneToOne(() => UserModel)
+    @JoinColumn({ name: 'userId' })
+    user: UserModel;
+
+    @Column({ type: 'jsonb', nullable: true })
+    preferences?: {
+        booking?: {
+            preferredBarberId?: string;
+            preferredServicesIds?: string[];
+            sendReminder: boolean;
+        };
+        service?: {
+            chatLevel: 'QUIET' | 'NORMAL' | 'CHATTY';
+            allergiesOrSensitivities?: string;
+            generalNotes?: string;
+        };
+    };
+
+    @OneToMany(() => Appointment, (appointment) => appointment.clientId)
+    appointments: Appointment[];
+}
