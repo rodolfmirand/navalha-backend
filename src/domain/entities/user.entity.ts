f3ef29de9c7f913d56b950b680ee1342a03eef21
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-06-13 18:10:32 -0300

    feat(domain): add constructor in user plain class

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -2,10 +2,18 @@ import { UserRole } from "../enums/user-role.enum";
 
 export class User {
     id: string;
+    name: string;
     email: string;
     passwordHash: string;
+    phoneNumber: string;
     role: UserRole;
 
-    name: string;
-    phoneNumber: string;
+    constructor(id: string, name: string, email: string, passwordHash: string, phoneNumber: string, role: UserRole,) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
 }
