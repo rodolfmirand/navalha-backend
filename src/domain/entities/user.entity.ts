ee3fd342df8d27cefaf77bdc8e16a3e4f0e6dc7c
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-06-16 07:29:13 -0300

    feat(domain): remove user plain class constructor

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -7,13 +7,4 @@ export class User {
     passwordHash: string;
     phoneNumber: string;
     role: UserRole;
-
-    constructor(id: string, name: string, email: string, passwordHash: string, phoneNumber: string, role: UserRole,) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.passwordHash = passwordHash;
-        this.phoneNumber = phoneNumber;
-        this.role = role;
-    }
 }
