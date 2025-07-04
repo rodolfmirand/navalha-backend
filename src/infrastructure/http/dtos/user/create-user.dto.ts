368d91c19447af9a29f9b43db468082801b491e4
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-04 07:36:49 -0300

    feat(dto): add create user dto class

---
+++ src/infrastructure/http/dtos/user/create-user.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class CreateUserDto {
+    @IsString()
+    @IsNotEmpty()
+    readonly name: string;
+
+    @IsEmail()
+    @IsNotEmpty()
+    readonly email: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(8, { message: 'Password must have at least 8 characters.' })
+    readonly passwordHash: string;
+}
