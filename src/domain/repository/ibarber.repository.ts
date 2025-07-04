80c2af8192f40f9016b59742dd060de0bcffda31
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-04 10:21:16 -0300

    feat: add barber repository interface

---
+++ src/domain/repository/ibarber.repository.ts
@@ -0,0 +1,8 @@
+import { Barber } from "../entities/barber.entity";
+
+export interface IBarberRepository {
+    save(barber: Barber): Promise<void>;
+    findById(id: string): Promise<Barber | null>;
+    findAll(): Promise<Barber[]>;
+    delete(id: string): Promise<void>;
+}
