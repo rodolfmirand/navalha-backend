4e0efb061a2813e5b8d2035b4be84449eeb101f0
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-04 11:05:31 -0300

    refactor(mapper): rename client mapper to customer

---
+++ src/infrastructure/persistence/mappers/customer.mapper.ts
@@ -0,0 +1,35 @@
+import { Customer } from 'src/domain/entities/customer.entity';
+import { CustomerModel } from '../typeorm/models/customer.model';
+import { AppointmentMapper } from './appointment.mapper';
+
+export class customerMapper {
+  public static toDomain(model: CustomerModel): Customer {
+    const entity = new Customer();
+
+    entity.id = model.id;
+    entity.userId = model.userId;
+
+    if (model.preferences) {
+      entity.preferences = model.preferences;
+    }
+
+    if (model.appointments) {
+      entity.appointments = model.appointments.map((appointmentModel) => AppointmentMapper.toDomain(appointmentModel));
+    }
+
+    return entity;
+  }
+
+  public static toPersistence(entity: Customer): CustomerModel {
+    const model = new CustomerModel();
+
+    model.id = entity.id;
+    model.userId = entity.userId;
+
+    if (entity.preferences) {
+      model.preferences = entity.preferences;
+    }
+
+    return model;
+  }
+}
