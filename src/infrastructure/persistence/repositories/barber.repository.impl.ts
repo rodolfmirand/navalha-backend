21b42728c32a7d105ddd139cff98ebe1502f94f1
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-07 11:51:37 -0300

    feat(infra): implement barber typeorm repository

--- src/infrastructure/persistence/repositories/barber.repository.impl.ts
+++ src/infrastructure/persistence/repositories/barber.repository.impl.ts
@@ -1,18 +1,32 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
 import { Barber } from "src/domain/entities/barber.entity";
 import { IBarberRepository } from "src/domain/repositories/ibarber.repository";
+import { BarberModel } from '../typeorm/models/barber.model';
+import { Repository } from "typeorm";
+import { BarberMapper } from "../mappers/barber.mapper";
 
+@Injectable()
 export class BarberRepositoryImpl implements IBarberRepository {
-    save(barber: Barber): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    findById(id: string): Promise<Barber | null> {
-        throw new Error("Method not implemented.");
-    }
-    findAll(): Promise<Barber[]> {
-        throw new Error("Method not implemented.");
-    }
-    delete(id: string): Promise<void> {
-        throw new Error("Method not implemented.");
+
+    constructor(@InjectRepository(BarberModel) private readonly repository: Repository<BarberModel>) { }
+
+    async save(barber: Barber): Promise<void> {
+        const model = BarberMapper.toPersistence(barber);
+        await this.repository.save(model);
     }
 
+    async findById(id: string): Promise<Barber | null> {
+        const model = await this.repository.findOneBy({ id });
+        return model ? BarberMapper.toDomain(model) : null;
+    }
+
+    async findAll(): Promise<Barber[]> {
+        const entities = await this.repository.find();
+        return entities.map(BarberMapper.toDomain);
+    }
+
+    async delete(id: string): Promise<void> {
+        await this.repository.delete({ id });
+    }
 }
