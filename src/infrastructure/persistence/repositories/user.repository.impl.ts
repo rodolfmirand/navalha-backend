0a7f7d4117b151e366aeb8c6a07fdb51badc7ca5
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-07 11:39:14 -0300

    feat(infra): implement user typeorm repository

--- src/infrastructure/persistence/repositories/user.repository.impl.ts
+++ src/infrastructure/persistence/repositories/user.repository.impl.ts
@@ -1,17 +1,32 @@
 import { User } from 'src/domain/entities/user.entity';
 import { IUserRepository } from '../../../domain/repositories/iuser.repository';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from "@nestjs/typeorm";
+import { UserModel } from '../typeorm/models/user.model';
+import { Repository } from 'typeorm';
+import { UserMapper } from '../mappers/user.mapper';
 
+@Injectable()
 export class UserRepositoryImpl implements IUserRepository {
-    save(user: User): Promise<void> {
-        throw new Error('Method not implemented.');
+
+    constructor(@InjectRepository(UserModel) private readonly repository: Repository<UserModel>) { }
+
+    async save(user: User): Promise<void> {
+        const userModel = UserMapper.toDomain(user);
+        await this.repository.save(userModel);
     }
-    findById(id: string): Promise<User | null> {
-        throw new Error('Method not implemented.');
+
+    async findById(id: string): Promise<User | null> {
+        const userModel = await this.repository.findOneBy({ id });
+        if (userModel == null) return null;
+        return UserMapper.toDomain(userModel);
     }
-    findAll(): Promise<User[]> {
-        throw new Error('Method not implemented.');
+
+    async findAll(): Promise<User[]> {
+        return await this.findAll();
     }
-    delete(id: string): Promise<void> {
-        throw new Error('Method not implemented.');
+
+    async delete(id: string): Promise<void> {
+        await this.repository.delete({ id });
     }
 }
