403c0e6d80c770497535fa78183c7bd31f18f5f9
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-08 08:18:43 -0300

    feat(infra): implement appointment typeorm repository

--- src/infrastructure/persistence/repositories/appointment.repository.impl.ts
+++ src/infrastructure/persistence/repositories/appointment.repository.impl.ts
@@ -1,17 +1,32 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
 import { Appointment } from "src/domain/entities/appointment.entity";
 import { IAppointmentRepository } from "src/domain/repositories/iappointment.repository";
+import { AppointmentModel } from "../typeorm/models/appointment.model";
+import { Repository } from "typeorm";
+import { AppointmentMapper } from "../mappers/appointment.mapper";
 
+@Injectable()
 export class AppointmentRepositoryImpl implements IAppointmentRepository {
-    save(appointment: Appointment): Promise<void> {
-        throw new Error("Method not implemented.");
+
+    constructor(@InjectRepository(AppointmentModel) private readonly repository: Repository<AppointmentModel>) { }
+
+    async save(appointment: Appointment): Promise<void> {
+        const model = AppointmentMapper.toPersistence(appointment);
+        await this.repository.save(model);
     }
-    findById(id: string): Promise<Appointment> {
-        throw new Error("Method not implemented.");
+
+    async findById(id: string): Promise<Appointment | null> {
+        const entity = await this.repository.findOneBy({ id });
+        return entity ? AppointmentMapper.toDomain(entity) : null;
     }
-    findAll(): Promise<Appointment[]> {
-        throw new Error("Method not implemented.");
+
+    async findAll(): Promise<Appointment[]> {
+        const entities = await this.repository.find();
+        return entities.map(AppointmentMapper.toDomain);
     }
-    delete(id: string): Promise<void> {
-        throw new Error("Method not implemented.");
+
+    async delete(id: string): Promise<void> {
+        await this.repository.delete({ id });
     }
 }
