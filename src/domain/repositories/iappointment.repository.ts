fe9242a67321a6a2979e6475c6d4619a5d585067
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-08 08:23:12 -0300

    refactor(domain): change findById method to return entity or null

--- src/domain/repositories/iappointment.repository.ts
+++ src/domain/repositories/iappointment.repository.ts
@@ -2,7 +2,7 @@ import { Appointment } from "../entities/appointment.entity";
 
 export interface IAppointmentRepository {
     save(appointment: Appointment): Promise<void>;
-    findById(id: string): Promise<Appointment>;
+    findById(id: string): Promise<Appointment | null>;
     findAll(): Promise<Appointment[]>;
     delete(id: string): Promise<void>;
 }
