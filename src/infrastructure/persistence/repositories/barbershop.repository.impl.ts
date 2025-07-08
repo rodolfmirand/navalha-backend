201948549ec1cd8d9ab5d7a4cb3b7f583735be6d
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-08 08:19:03 -0300

    feat(infra): implement appointment typeorm repository

--- src/infrastructure/persistence/repositories/barbershop.repository.impl.ts
+++ src/infrastructure/persistence/repositories/barbershop.repository.impl.ts
@@ -1,18 +1,32 @@
-import { Barber } from "src/domain/entities/barber.entity";
-import { IBarberRepository } from "src/domain/repositories/ibarber.repository";
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { BarbershopModel } from "../typeorm/models/barbershop.model";
+import { Repository } from "typeorm";
+import { BarbershopMapper } from "../mappers/barbershop.mapper";
+import { IBarbershopRepository } from "src/domain/repositories/ibarbershop.repository";
+import { Barbershop } from "src/domain/entities/barbershop.entity";
 
-export class BarbershopRepositoryImpl implements IBarberRepository {
-    save(barber: Barber): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    findById(id: string): Promise<Barber | null> {
-        throw new Error("Method not implemented.");
-    }
-    findAll(): Promise<Barber[]> {
-        throw new Error("Method not implemented.");
-    }
-    delete(id: string): Promise<void> {
-        throw new Error("Method not implemented.");
+@Injectable()
+export class BarbershopRepositoryImpl implements IBarbershopRepository {
+
+    constructor(@InjectRepository(BarbershopModel) private readonly repository: Repository<BarbershopModel>) { }
+
+    async save(barbershop: Barbershop): Promise<void> {
+        const model = BarbershopMapper.toPersistence(barbershop);
+        await this.repository.save(model);
     }
 
+    async findById(id: string): Promise<Barbershop | null> {
+        const entity = await this.repository.findOneBy({ id });
+        return entity ? BarbershopMapper.toDomain(entity) : null;
+    }
+
+    async findAll(): Promise<Barbershop[]> {
+        const entities = await this.repository.find();
+        return entities.map(BarbershopMapper.toDomain);
+    }
+
+    async delete(id: string): Promise<void> {
+        await this.repository.delete({ id });
+    }
 }
