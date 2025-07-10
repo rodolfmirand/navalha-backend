f5d12a4d680404cb04e0e5b13517641e6022c8b2
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-10 20:06:26 -0300

    feat(infra/service): add create customer service

---
+++ src/infrastructure/persistence/services/customer/create-customer.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Customer } from "src/domain/entities/customer.entity";
+import { ICustomerRepository } from "src/domain/repositories/icustomer.repository";
+import { IUserRepository } from "src/domain/repositories/iuser.repository";
+
+@Injectable()
+export class CreateCustomerService {
+
+    constructor(private readonly customerRepository: ICustomerRepository, private readonly userRepository: IUserRepository) { }
+
+    async execute(customer: Customer): Promise<void> {
+        const user = await this.userRepository.findById(customer.userId);
+        if(!user) {
+            throw new NotFoundException("User not found.");
+        }
+
+        await this.customerRepository.save(customer);
+    }
+}
