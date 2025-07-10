c32dbefc28d069aa1ee671fd775869ca059941a9
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-10 20:06:53 -0300

    feat(infra/service): add find all customer service

---
+++ src/infrastructure/persistence/services/customer/find-all-customers.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from "@nestjs/common";
+import { Customer } from "src/domain/entities/customer.entity";
+import { ICustomerRepository } from "src/domain/repositories/icustomer.repository";
+
+@Injectable()
+export class FindAllUsersService {
+
+    constructor(private readonly repository: ICustomerRepository) { }
+
+    async execute(): Promise<Customer[]> {
+        return await this.repository.findAll();
+    }
+}
