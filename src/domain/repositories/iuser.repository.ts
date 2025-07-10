bb67f5a2b6678ada1625d148e5e2e68f78cfb98f
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-10 11:26:56 -0300

    feat(domain): add findByEmail and findByPhoneNumber methods in user repository interface

--- src/domain/repositories/iuser.repository.ts
+++ src/domain/repositories/iuser.repository.ts
@@ -3,6 +3,8 @@ import { User } from '../entities/user.entity';
 export interface IUserRepository {
     save(user: User): Promise<void>;
     findById(id: string): Promise<User | null>;
+    findByEmail(email: string): Promise<User | null>;
+    findByPhoneNumber(phoneNumber: string): Promise<User | null>;
     findAll(): Promise<User[]>;
     delete(id: string): Promise<void>;
 }
