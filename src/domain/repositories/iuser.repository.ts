9db95475cc967784a3887716981049d143d8afd3
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-10 20:05:08 -0300

    refactor(domain): make create user method from repository interface return user entity

--- src/domain/repositories/iuser.repository.ts
+++ src/domain/repositories/iuser.repository.ts
@@ -1,7 +1,7 @@
 import { User } from '../entities/user.entity';
 
 export interface IUserRepository {
-    save(user: User): Promise<void>;
+    save(user: User): Promise<User>;
     findById(id: string): Promise<User | null>;
     findByEmail(email: string): Promise<User | null>;
     findByPhoneNumber(phoneNumber: string): Promise<User | null>;
