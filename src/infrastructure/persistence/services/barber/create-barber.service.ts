daa2a2d90dc875182313f47464a80fc2b915c5da
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-11 10:48:23 -0300

    feat(infra/service): add create barber service

---
+++ src/infrastructure/persistence/services/barber/create-barber.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Barber } from "src/domain/entities/barber.entity";
+import { IBarberRepository } from "src/domain/repositories/ibarber.repository";
+import { IUserRepository } from "src/domain/repositories/iuser.repository";
+
+@Injectable()
+export class CreateBarberService {
+
+    constructor(private readonly barberRepository: IBarberRepository, private readonly userRepository: IUserRepository) { }
+
+    async execute(barber: Barber): Promise<void> {
+        const user = await this.userRepository.findById(barber.userId);
+        if (!user) {
+            throw new NotFoundException("User not found");
+        }
+
+        await this.barberRepository.save(barber);
+    }
+}
