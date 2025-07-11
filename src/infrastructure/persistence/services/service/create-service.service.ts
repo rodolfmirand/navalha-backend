a916fb71358675f3c10e8d3058b29f5653127216
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-11 11:11:59 -0300

    feat(infra/service): add create service service

---
+++ src/infrastructure/persistence/services/service/create-service.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Service } from "src/domain/entities/service.entity";
+import { IBarbershopRepository } from "src/domain/repositories/ibarbershop.repository";
+import { IServiceRepository } from "src/domain/repositories/iservice.repository";
+
+@Injectable()
+export class CreateServiceService {
+
+    constructor(private readonly serviceRepository: IServiceRepository, private readonly barbershopRepository: IBarbershopRepository) { }
+
+    async execute(service: Service): Promise<void> {
+        const barbershop = await this.barbershopRepository.findById(service.barbershopId);
+        if (!barbershop) {
+            throw new NotFoundException("Barber Shop not found.");
+        }
+
+        await this.serviceRepository.save(service);
+    }
+}
