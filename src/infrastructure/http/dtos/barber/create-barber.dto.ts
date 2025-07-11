51a055818187e9f5e38b59607a83e285b124bf83
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-11 10:47:10 -0300

    feat(dto): add user id in create barber dto

--- src/infrastructure/http/dtos/barber/create-barber.dto.ts
+++ src/infrastructure/http/dtos/barber/create-barber.dto.ts
@@ -1,10 +1,15 @@
-import { IsArray, IsOptional, IsString, IsUUID } from "class-validator";
+import { IsArray, IsNotEmpty, IsOptional, IsString, IsUUID } from "class-validator";
 
 export class CreateBarberDto {
     @IsUUID('4')
+    @IsNotEmpty()
     readonly barbershopId: string;
 
+    @IsUUID('4')
+    readonly userId: string;
+
     @IsArray()
+    @IsOptional()
     readonly specialties: string[];
 
     @IsOptional()
