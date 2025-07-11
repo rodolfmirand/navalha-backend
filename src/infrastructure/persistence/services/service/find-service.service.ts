a0d97533c1620aad30cc64becb4277532816275d
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-11 11:12:10 -0300

    feat(infra/service): add find service service

---
+++ src/infrastructure/persistence/services/service/find-service.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Service } from "src/domain/entities/service.entity";
+import { IServiceRepository } from "src/domain/repositories/iservice.repository";
+
+@Injectable()
+export class FindServiceService {
+
+    constructor(private readonly repository: IServiceRepository) { }
+
+    async execute(id: string): Promise<Service> {
+        const service = await this.repository.findById(id);
+        if (!service) {
+            throw new NotFoundException("Service not found.")
+        }
+
+        return service;
+    }
+}
