a899372aa215d7b4d1938e456ecf50d36e931b85
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-14 12:05:31 -0300

    feat(controller): add find user endpoint

--- src/infrastructure/http/controllers/user.controller.ts
+++ src/infrastructure/http/controllers/user.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 import { FindAllUsersService } from "src/application/services/customer/find-all-customers.service";
 import { CreateUserService } from "src/application/services/user/create-user.service";
 import { DeleteUserService } from "src/application/services/user/delete-user.service";
 import { FindUserService } from "src/application/services/user/find-user.service";
 import { CreateUserDto } from "../dtos/user/create-user.dto";
 import { UserMapper } from "src/infrastructure/persistence/mappers/user.mapper";
+import { UserRespondeDto } from "../dtos/user/user-responde.dto";
 
 @Controller('user')
 export class UserController {
@@ -19,5 +20,11 @@ export class UserController {
     async create(@Body() dto: CreateUserDto): Promise<void> {
         const user = UserMapper.fromDTO(dto);
         await this.createUser.execute(user);
+    }
+
+    @Get(':id')
+    async find(@Param('id') id: string): Promise<UserRespondeDto> {
+        const user = await this.findUser.execute(id);
+        return UserMapper.toRespondeDTO(user);
     }
 }
