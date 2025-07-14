dc2ca84ee8c438f1393bbf9103a308b284f1472e
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-14 10:10:07 -0300

    feat(infra/services): add delete appointment service

---
+++ src/infrastructure/persistence/services/appointment/delete-appointment.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { IAppointmentRepository } from "src/domain/repositories/iappointment.repository";
+
+@Injectable()
+export class DeleteAppointmentService {
+
+    constructor(private readonly repository: IAppointmentRepository) { }
+
+    async execute(id: string): Promise<void> {
+        const appointment = await this.repository.findById(id);
+        if (!appointment) {
+            throw new NotFoundException("Appointment not found.");
+        }
+
+        await this.repository.delete(id);
+    }
+}
