be1853b7f8b34ac7dc7acd7678d1e90bbda1c8d7
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-14 10:54:16 -0300

    feat(mappers): add fromDTO mapper method

--- src/infrastructure/persistence/mappers/user.mapper.ts
+++ src/infrastructure/persistence/mappers/user.mapper.ts
@@ -1,5 +1,6 @@
 import { User } from 'src/domain/entities/user.entity';
 import { UserModel } from '../typeorm/models/user.model';
+import { CreateUserDto } from '../../http/dtos/user/create-user.dto';
 export class UserMapper {
     public static toDomain(model: UserModel): User {
         const entity = new User();
@@ -7,7 +8,7 @@ export class UserMapper {
         entity.id = model.id;
         entity.name = model.name;
         entity.email = model.email;
-        entity.passwordHash = model.passwordHash;
+        entity.password = model.passwordHash;
         entity.phoneNumber = model.phoneNumber;
         entity.role = model.role;
 
@@ -20,10 +21,22 @@ export class UserMapper {
         model.id = entity.id;
         model.name = entity.name;
         model.email = entity.email;
-        model.passwordHash = entity.passwordHash;
+        model.passwordHash = entity.password;
         model.phoneNumber = entity.phoneNumber;
         model.role = entity.role;
 
         return model;
+    }
+
+    public static fromDTO(dto: CreateUserDto): User {
+        const entity = new User();
+
+        entity.id = "";
+        entity.name = dto.name;
+        entity.email = dto.email;
+        entity.phoneNumber = dto.phoneNumber;
+        entity.password = dto.password;
+
+        return entity;
     }
 }
