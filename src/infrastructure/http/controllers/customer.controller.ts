26a59fb122bc1d405dd5935c43c43d05565e8153
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-17 10:37:29 -0300

    feat(controller): implement find, findAll and delete customer

--- src/infrastructure/http/controllers/customer.controller.ts
+++ src/infrastructure/http/controllers/customer.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
 import { CreateCustomerService } from "src/application/services/customer/create-customer.service";
 import { DeleteCustomerService } from "src/application/services/customer/delete-customer.service";
 import { FindAllCUstomersService } from "src/application/services/customer/find-all-customers.service";
@@ -21,5 +21,22 @@ export class CustomerController {
         const customer = CustomerMapper.fromDTO(dto);
         const savedCustomer = await this.createCustomer.execute(customer);
         return CustomerMapper.toDTO(savedCustomer);
+    }
+
+    @Get(':id')
+    async find(@Param('id') id: string): Promise<CustomerResponseDto> {
+        const customer = await this.findCustomer.execute(id);
+        return CustomerMapper.toDTO(customer);
+    }
+
+    @Get()
+    async findAll(): Promise<CustomerResponseDto[]> {
+        const customers = await this.findAllCustomers.execute();
+        return customers.map(CustomerMapper.toDTO);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: string): Promise<void> {
+        await this.deleteCustomer.execute(id);
     }
 }
