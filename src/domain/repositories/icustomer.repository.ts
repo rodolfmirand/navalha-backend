330c2171df847e5983ba395e31622065567b145a
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-17 10:27:16 -0300

    refactor(domain/repository): make save method return customer model

--- src/domain/repositories/icustomer.repository.ts
+++ src/domain/repositories/icustomer.repository.ts
@@ -1,7 +1,7 @@
 import { Customer } from "../entities/customer.entity";
 
 export interface ICustomerRepository {
-    save(customer: Customer): Promise<void>;
+    save(customer: Customer): Promise<Customer>;
     findById(id: string): Promise<Customer | null>;
     findAll(): Promise<Customer[]>;
     delete(id: string): Promise<void>;
