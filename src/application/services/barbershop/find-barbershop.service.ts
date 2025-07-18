cad8815d3c488e8c5f5f0a3ed8874a8d8f01036e
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-18 11:17:27 -0300

    feat(service): add find barbershop service

---
+++ src/application/services/barbershop/find-barbershop.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Barbershop } from "src/domain/entities/barbershop.entity";
+import { IBarbershopRepository } from "src/domain/repositories/ibarbershop.repository";
+
+@Injectable()
+export class FindBarbershopService {
+
+    constructor(private readonly repository: IBarbershopRepository) { }
+
+    async execute(id: string): Promise<Barbershop> {
+        const barbershop = await this.repository.findById(id);
+        if (!barbershop) {
+            throw new NotFoundException("Barber shop not found.");
+        }
+
+        return barbershop;
+    }
+}
