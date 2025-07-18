72d60a54a23f7889bb1821914486c67e1b8d4a5b
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-18 11:18:19 -0300

    refactor(domain/repository): make save barbershop return domain entity class

--- src/domain/repositories/ibarbershop.repository.ts
+++ src/domain/repositories/ibarbershop.repository.ts
@@ -1,7 +1,7 @@
 import { Barbershop } from "../entities/barbershop.entity";
 
 export interface IBarbershopRepository {
-    save(barbershop: Barbershop): Promise<void>;
+    save(barbershop: Barbershop): Promise<Barbershop>;
     findById(id: string): Promise<Barbershop | null>;
     findAll(): Promise<Barbershop[]>;
     delete(id: string): Promise<void>;
