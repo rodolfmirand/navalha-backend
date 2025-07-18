a24fa7eb4ef1ae3d56b512b53c157496fac8dda0
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-18 07:19:28 -0300

    refactor(domain/repository): make save barber method return entity domain class

--- src/domain/repositories/ibarber.repository.ts
+++ src/domain/repositories/ibarber.repository.ts
@@ -1,7 +1,7 @@
 import { Barber } from "../entities/barber.entity";
 
 export interface IBarberRepository {
-    save(barber: Barber): Promise<void>;
+    save(barber: Barber): Promise<Barber>;
     findById(id: string): Promise<Barber | null>;
     findAll(): Promise<Barber[]>;
     delete(id: string): Promise<void>;
