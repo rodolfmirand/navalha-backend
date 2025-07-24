90c33dbe9ceedc739bb312a6455faab5fa0bd6ac
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-24 08:14:52 -0300

    feat(mapper): fromDTO and toDTO to operating hours mapper

--- src/infrastructure/persistence/mappers/operating-hours.mapper.ts
+++ src/infrastructure/persistence/mappers/operating-hours.mapper.ts
@@ -1,5 +1,7 @@
 import { OperatingHours } from 'src/domain/entities/operating-hours.entity';
 import { OperatingHoursModel } from '../typeorm/models/operating-hours.model';
+import { OperatingHoursDto } from 'src/infrastructure/http/dtos/barbershop/operating-hours/operating-hours.dto';
+import { OperatingHoursResponseDto } from 'src/infrastructure/http/dtos/barbershop/operating-hours/operating-hours-response.dto';
 export class OperatingHoursMapper {
     public static toDomain(model: OperatingHoursModel): OperatingHours {
         const entity = new OperatingHours();
@@ -21,7 +23,28 @@ export class OperatingHoursMapper {
         model.openingTime = entity.openingTime;
         model.closingTime = entity.closingTime;
         model.barbershopId = entity.barbershopId;
-        
+
         return model;
+    }
+
+    public static fromDTO(dto: OperatingHoursDto): OperatingHours {
+        const entity = new OperatingHours();
+
+        entity.dayOfWeek = dto.dayOfWeek;
+        entity.openingTime = dto.openingTime;
+        entity.closingTime = dto.closingTime;
+
+        return entity;
+    }
+
+    public static toDTO(entity: OperatingHours): OperatingHoursResponseDto {
+        return {
+            id: entity.id,
+            barbershopId: entity.barbershopId,
+
+            dayOfWeek: entity.dayOfWeek,
+            openingTime: entity.openingTime,
+            closingTime: entity.closingTime
+        }
     }
 }
