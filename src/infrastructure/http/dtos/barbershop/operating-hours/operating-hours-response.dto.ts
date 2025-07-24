7c9b0710b2a7c815c49ae6211b03843281db269c
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-24 08:12:52 -0300

    feat(dto): add id and barbershopId to operating hours response dto

--- src/infrastructure/http/dtos/barbershop/operating-hours/operating-hours-response.dto.ts
+++ src/infrastructure/http/dtos/barbershop/operating-hours/operating-hours-response.dto.ts
@@ -2,9 +2,12 @@ import { IsEnum } from "class-validator";
 import { DayOfWeek } from "src/domain/enums/day-of-week";
 
 export class OperatingHoursResponseDto {
+    readonly id: string;
+    readonly barbershopId: string;
+    
     @IsEnum(DayOfWeek)
     readonly dayOfWeek: DayOfWeek;
-    
+
     readonly openingTime: string;
     readonly closingTime: string;
 }
