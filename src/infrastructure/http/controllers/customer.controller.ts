a044c6834c8ecd545c4a95e7f2b19586ca5107f4
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-30 08:28:49 -0300

    feat(controller): implement update customer endpoint

--- src/infrastructure/http/controllers/customer.controller.ts
+++ src/infrastructure/http/controllers/customer.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { CreateCustomerService } from "src/application/services/customer/create-customer.service";
 import { DeleteCustomerService } from "src/application/services/customer/delete-customer.service";
 import { FindAllCUstomersService } from "src/application/services/customer/find-all-customers.service";
@@ -6,6 +6,8 @@ import { FindCustomerService } from "src/application/services/customer/find-cust
 import { CreateCustomerDto } from "../dtos/customer/create-customer.dto";
 import { CustomerResponseDto } from "../dtos/customer/customer-response.dto";
 import { CustomerMapper } from "src/infrastructure/persistence/mappers/customer.mapper";
+import { UpdateCustomerDto } from "../dtos/customer/update-customer.dto";
+import { UpdateCustomerService } from "src/application/services/customer/update-customer.service";
 
 @Controller('customer')
 export class CustomerController {
@@ -13,12 +15,13 @@ export class CustomerController {
     constructor(private readonly createCustomer: CreateCustomerService,
         private readonly findCustomer: FindCustomerService,
         private readonly findAllCustomers: FindAllCUstomersService,
-        private readonly deleteCustomer: DeleteCustomerService
+        private readonly deleteCustomer: DeleteCustomerService,
+        private readonly updateCustomer: UpdateCustomerService
     ) { }
 
     @Post()
     async create(@Body() dto: CreateCustomerDto): Promise<CustomerResponseDto> {
-        const customer = CustomerMapper.fromDTO(dto);
+        const customer = CustomerMapper.fromCreateDTO(dto);
         const savedCustomer = await this.createCustomer.execute(customer);
         return CustomerMapper.toDTO(savedCustomer);
     }
@@ -38,5 +41,12 @@ export class CustomerController {
     @Delete(':id')
     async delete(@Param('id') id: string): Promise<void> {
         await this.deleteCustomer.execute(id);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id: string, @Body() dto: UpdateCustomerDto): Promise<CustomerResponseDto> {
+        const customer = CustomerMapper.fromUpdateDTO(dto);
+        const updatedCustomer = await this.updateCustomer.execute(id, customer);
+        return CustomerMapper.toDTO(updatedCustomer);
     }
 }
