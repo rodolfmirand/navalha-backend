699d6780a10fe81d9c2f1fd4706a04bd7e945310
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-30 08:28:30 -0300

    refactor(dto): remove user atributes from customer update dto

--- src/infrastructure/http/dtos/customer/update-customer.dto.ts
+++ src/infrastructure/http/dtos/customer/update-customer.dto.ts
@@ -1,31 +1,8 @@
-import { IsString, IsNotEmpty, IsEmail, MinLength, IsOptional, ValidateNested } from "class-validator";
+import { ValidateNested } from "class-validator";
 import { CustomerPreferencesDto } from './customer-preferences/customer-preferences.dto';
 import { Type } from "class-transformer";
 
 export class UpdateCustomerDto {
-    @IsOptional()
-    @IsString()
-    @IsNotEmpty()
-    name: string;
-
-    @IsOptional()
-    @IsEmail()
-    @IsNotEmpty()
-    email: string;
-
-    @IsOptional()
-    @IsString()
-    @IsNotEmpty()
-    @MinLength(8, { message: 'Password must have at least 8 characters.' })
-    passwordHash: string;
-
-    @IsOptional()
-    @IsString()
-    @IsNotEmpty()
-    @MinLength(10)
-    phoneNumber: string;
-
-    @IsOptional()
     @ValidateNested()
     @Type(() => CustomerPreferencesDto)
     preferences?: CustomerPreferencesDto;
