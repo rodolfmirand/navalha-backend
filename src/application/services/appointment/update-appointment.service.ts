d1c518443b81fb7828e14870431709f3fdea36f2
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-07-31 09:44:39 -0300

    feat(service): add update appointment service

---
+++ src/application/services/appointment/update-appointment.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { AppointmentStatus } from "src/domain/enums/appointment-status.enum";
+import { IAppointmentRepository } from "src/domain/repositories/iappointment.repository";
+
+@Injectable()
+export class UpdateAppointmentService {
+    constructor(private readonly repository: IAppointmentRepository) { }
+
+    async execute(id: string, status: AppointmentStatus): Promise<void> {
+        const appointment = await this.repository.findById(id);
+        if(!appointment) {
+            throw new NotFoundException("Appointment not found.");
+        }
+
+        appointment.status = status;
+    }
+}
