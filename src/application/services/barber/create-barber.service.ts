2b04e71b0ebc64e3b23254b59c45c514593d7c1e
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-08-11 11:36:54 -0300

    feat(service): add verification to type of user and if barbershop exists

--- src/application/services/barber/create-barber.service.ts
+++ src/application/services/barber/create-barber.service.ts
@@ -1,17 +1,29 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { Barber } from "src/domain/entities/barber.entity";
-import { IBarberRepository } from "src/domain/repositories/ibarber.repository";
-import { IUserRepository } from "src/domain/repositories/iuser.repository";
+import { UserRole } from "src/domain/enums/user-role.enum";
+import { BarberRepositoryImpl } from "src/infrastructure/persistence/repositories/barber.repository.impl";
+import { BarbershopRepositoryImpl } from "src/infrastructure/persistence/repositories/barbershop.repository.impl";
+import { UserRepositoryImpl } from "src/infrastructure/persistence/repositories/user.repository.impl";
 
 @Injectable()
 export class CreateBarberService {
 
-    constructor(private readonly barberRepository: IBarberRepository, private readonly userRepository: IUserRepository) { }
+    constructor(private readonly barberRepository: BarberRepositoryImpl,
+        private readonly userRepository: UserRepositoryImpl,
+        private readonly barbershopRepository: BarbershopRepositoryImpl) { }
 
     async execute(barber: Barber): Promise<Barber> {
         const user = await this.userRepository.findById(barber.userId);
         if (!user) {
-            throw new NotFoundException("User not found");
+            throw new NotFoundException("User not found.");
+        }
+        if (user.role !== UserRole.BARBER) {
+            throw new BadRequestException("User must be a barber.");
+        }
+
+        const barbershop = await this.barbershopRepository.findById(barber.barbershopId);
+        if (!barbershop) {
+            throw new NotFoundException("Barbershop not found.")
         }
 
         return await this.barberRepository.save(barber);
