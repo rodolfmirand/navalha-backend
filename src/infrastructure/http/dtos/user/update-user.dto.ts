b48b4a1f03cb5ac499619d67d2fe7a6828c1b959
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-08-14 09:36:00 -0300

    refactor(dto): rename update user dto

---
+++ src/infrastructure/http/dtos/user/update-user.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsPhoneNumber, IsString } from "class-validator";
+import { UserRole } from "src/domain/enums/user-role.enum";
+
+export class UpdateUserDto {
+    @IsOptional()
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsOptional()
+    @IsEmail()
+    @IsNotEmpty()
+    email: string;
+
+    @IsOptional()
+    @IsPhoneNumber('BR')
+    @IsNotEmpty()
+    phoneNumber: string;
+
+    @IsEnum(UserRole)
+    @IsOptional()
+    role: UserRole;
+}
