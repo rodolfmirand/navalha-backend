3abe2f9033c7e535fc3d93fdd414e80d46203e82
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-08-22 11:39:50 -0300

    refactor(mapper): map flattened booking and service fields from customer entity

--- src/infrastructure/persistence/mappers/customer-preferences.mapper.ts
+++ src/infrastructure/persistence/mappers/customer-preferences.mapper.ts
@@ -2,10 +2,18 @@ import { CustomerPreferencesResponseDto } from 'src/infrastructure/http/dtos/cus
 import { Customer } from '../../../domain/entities/customer.entity';
 
 export class CustomerPreferencesMapper {
-    public static toDTO(preferences: Customer['preferences']): CustomerPreferencesResponseDto {
+    public static toDTO(entity: Customer): CustomerPreferencesResponseDto {
         return {
-            booking: preferences?.booking,
-            service: preferences?.service
+            booking: {
+                preferredBarberId: entity.preferredBarberId,
+                preferredServicesId: entity.preferredServicesId,
+                sendReminder: entity.sendReminder
+            },
+            service: {
+                allergiesOrSensitivities: entity.allergiesOrSensitivities,
+                chatLevel: entity.chatLevel,
+                generalNotes: entity.generalNotes
+            }
         }
     }
 }
