43d1b51aecee719b794b105d7db710d82b844d96
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-09-05 10:06:35 -0300

    feat(dto): add username field to UserResponseDTO for user details

--- src/infrastructure/http/dtos/user/user-response.dto.ts
+++ src/infrastructure/http/dtos/user/user-response.dto.ts
@@ -4,6 +4,7 @@ export class UserResponseDTO {
     readonly id: string;
     readonly name: string;
     readonly email: string;
+    readonly username: string;
     readonly phoneNumber: string;
     readonly role: UserRole;
 }
