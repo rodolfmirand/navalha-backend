c68a69c867a8f8da878466b7bc7eb3b2ee72117f
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-09-08 11:43:36 -0300

    feat(auth): inject refresh JWT options into AuthService and update login method to return refresh token

--- src/application/services/auth/auth.service.ts
+++ src/application/services/auth/auth.service.ts
@@ -1,15 +1,21 @@
-import { Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
+import { Inject, Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
 import { UserRepositoryImpl } from "src/infrastructure/persistence/repositories/user.repository.impl";
 import * as bcrypt from 'bcrypt';
 import { User } from "src/domain/entities/user.entity";
 import { UUID } from "crypto";
 import { JwtService } from "@nestjs/jwt";
 import { AuthJwtPayload } from "./types/auth-jwtPayload.type";
+import refreshJwtConfig from "src/application/config/refresh-jwt.config";
+import { ConfigType } from "@nestjs/config";
 
 @Injectable()
 export class AuthService {
 
-    constructor(private readonly userRepository: UserRepositoryImpl, private readonly jwtService: JwtService) { }
+    constructor(
+        private readonly userRepository: UserRepositoryImpl,
+        private readonly jwtService: JwtService,
+        @Inject(refreshJwtConfig.KEY) private readonly refreshJwtOptions: ConfigType<typeof refreshJwtConfig>,
+    ) { }
 
     async validateUser(login: string, password: string): Promise<User | null> {
         const isEmail = login.includes('@');
@@ -32,6 +38,22 @@ export class AuthService {
 
     login(userId: UUID) {
         const payload: AuthJwtPayload = { sub: userId }
-        return this.jwtService.sign(payload);
+        const token = this.jwtService.sign(payload);
+        const refreshToken = this.jwtService.sign(payload, this.refreshJwtOptions);
+
+        return {
+            userId: userId,
+            token, refreshToken
+        };
+    }
+
+    refreshToken(userId: UUID) {
+        const payload: AuthJwtPayload = { sub: userId }
+        const token = this.jwtService.sign(payload);
+        
+        return {
+            userId: userId,
+            token
+        }
     }
 }
