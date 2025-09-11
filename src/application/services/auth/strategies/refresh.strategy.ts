760a268c0cd6c8a1e24dd57c476a24aad113a34c
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-09-11 09:44:09 -0300

    feat(auth): enhance JwtStrategy to validate refresh tokens using request context

--- src/application/services/auth/strategies/refresh.strategy.ts
+++ src/application/services/auth/strategies/refresh.strategy.ts
@@ -4,18 +4,26 @@ import { ConfigType } from '@nestjs/config';
 import { AuthJwtPayload } from '../types/auth-jwtPayload.type';
 import { Inject, Injectable } from '@nestjs/common';
 import refreshJwtConfig from 'src/application/config/refresh-jwt.config';
+import { Request } from 'express';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'refresh-jwt') {
-    constructor(@Inject(refreshJwtConfig.KEY) private refreshJwtConfiguration: ConfigType<typeof refreshJwtConfig>) { 
+    constructor(@Inject(refreshJwtConfig.KEY) private refreshJwtConfiguration: ConfigType<typeof refreshJwtConfig>,
+        private readonly authService: AuthService
+    ) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: refreshJwtConfiguration.secret!,
             ignoreExpiration: false,
+            passReqToCallback: true,
         });
     }
 
-    validate(payload: AuthJwtPayload) {
-        return { id: payload.sub };
+    validate(req: Request, payload: AuthJwtPayload) {
+        const refreshToken = req.get("authorization")?.replace("Bearer", "").trim();
+        const userId = payload.sub;
+
+        return this.authService.validateRefreshToken(userId, refreshToken!);
     }
 }
