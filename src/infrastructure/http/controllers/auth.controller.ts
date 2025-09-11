071afb702f436afa336f979b26c91e2d16c30571
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-09-11 10:40:15 -0300

    feat(auth): add logout endpoint and replace AuthGuard from refresh endpoint to RefreshAuthGuard

--- src/infrastructure/http/controllers/auth.controller.ts
+++ src/infrastructure/http/controllers/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, HttpCode, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { AuthService } from '../../../application/services/auth/auth.service';
 import { LocalAuthGuard } from "src/application/services/auth/guards/local-auth.guard";
+import { JwtAuthGuard } from "src/application/services/auth/guards/jwt-auth.guard";
+import { RefreshAuthGuard } from "src/application/services/auth/guards/refresh-auth.guard";
 
 @Controller('auth')
 export class AuthController {
@@ -16,8 +18,14 @@ export class AuthController {
     }
 
     @Post('refresh')
-    @UseGuards(AuthGuard('refresh-jwt'))
+    @UseGuards(RefreshAuthGuard)
     async refresh(@Request() req) {
         return this.AuthService.refreshToken(req.user.id);
+    }
+
+    @Post('logout')
+    @UseGuards(JwtAuthGuard)
+    async logout(@Request() req) {
+        return this.AuthService.signOut(req.user.id);
     }
 }
